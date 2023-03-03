15e28f48ebf99d8e794d59003a3fb1a043e94bc5
ndudar <ndudar@gmail.com>
2023-03-02 18:26:44 -0600

    feat: adding to van page

--- pages/Vans.jsx
+++ pages/Vans.jsx
@@ -27,6 +27,7 @@ export default function Vans() {
 
   return (
     <div className="van-list-container">
+      <h1>Explore our van options</h1>
       <div className="van-list">{vanElements}</div>
     </div>
   );
