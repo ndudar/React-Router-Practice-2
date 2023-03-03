b4b241b7eff553541b1c7f87adb36cde692b69bd
ndudar <ndudar@gmail.com>
2023-03-03 14:01:36 -0600

    feat: individual van page

--- pages/VanDetail.jsx
+++ pages/VanDetail.jsx
@@ -1,7 +1,36 @@
-import React from "react"
+import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function VanDetail() {
+  //grab params from url
+  const params = useParams();
+
+  //state
+  const [van, setVan] = React.useState(null);
+
+  //grab the van from the mirage "server"
+  React.useEffect(() => {
+    fetch(`/api/vans/${params.id}`)
+      .then((res) => res.json())
+      .then((data) => setVan(data.vans));
+  }, [params.id]);
+
   return (
-    <h1>Van detail page goes here.</h1>
-  )
+    <div className="van-detail-container">
+      {van ? (
+        <div className="van-detail">
+          <img src={van.imageUrl} />
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
+          <h2>{van.name}</h2>
+          <p className="van-price">
+            <span>${van.price}</span>/day
+          </p>
+          <p>{van.description}</p>
+          <button className="link-button">Rent this van</button>
+        </div>
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </div>
+  );
 }
