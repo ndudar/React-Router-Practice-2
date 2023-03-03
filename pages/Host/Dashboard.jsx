4d23429c8181032a2a97284ed1c8871ebcc11204
ndudar <ndudar@gmail.com>
2023-03-03 16:13:24 -0600

    nesting host routes and building an outlet

--- pages/Host/Dashboard.jsx
+++ pages/Host/Dashboard.jsx
@@ -1,7 +1,11 @@
-import React from "react"
+import React from "react";
+import { Outlet } from "react-router-dom";
 
 export default function Dashboard() {
   return (
-    <h1>This is the host dashboard.</h1>
-  )
+    <>
+      <h1>This is the host dashboard.</h1>
+      <Outlet />
+    </>
+  );
 }
