358700288c94c2ea2d48ab3773a23e0b79f42424
ndudar <ndudar@gmail.com>
2023-03-05 18:17:25 -0600

    feat: host vans now loads state and styling done

--- pages/Host/HostVans.jsx
+++ pages/Host/HostVans.jsx
@@ -1,7 +1,49 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 export default function HostVans() {
-  return (
-    <h1>This is the host vans page.</h1>
-  )
+  //state
+  const [vans, setVans] = React.useState([])
+
+  //grabbing the vans from the mirage server that match the host id
+  //opportunity to optimize: vans in a cache so no refresh each time page loads
+  React.useEffect(() => {
+    fetch("/api/host/vans")
+    .then((res) => res.json())
+    .then((data) => setVans(data.vans))
+  }, [])
+
+  const hostVansEls = vans.map(van => (
+    <Link
+        to={`/host/vans/${van.id}`}
+        key={van.id}
+        className="host-van-link-wrapper"
+    >
+        <div className="host-van-single" key={van.id}>
+            <img src={van.imageUrl} alt={`Photo of ${van.name}`} />
+            <div className="host-van-info">
+                <h3>{van.name}</h3>
+                <p>${van.price}/day</p>
+            </div>
+        </div>
+    </Link>
+))
+
+return (
+    <section>
+        <h1 className="host-vans-title">Your listed vans</h1>
+        <div className="host-vans-list">
+            {
+                vans.length > 0 ? (
+                    <section>
+                        {hostVansEls}
+                    </section>
+
+                ) : (
+                        <h2>Loading...</h2>
+                    )
+            }
+        </div>
+    </section>
+)
 }
