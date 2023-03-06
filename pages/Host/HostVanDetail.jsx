9f74eda4b092608025045f742e79423b86247179
ndudar <ndudar@gmail.com>
2023-03-05 18:52:11 -0600

    feat: single host van rendering

--- pages/Host/HostVanDetail.jsx
+++ pages/Host/HostVanDetail.jsx
@@ -1,7 +1,34 @@
-import React from "react"
+import React from "react";
+import { useParams } from "react-router-dom";
 
 export default function HostVanDetail() {
+  //grab params from URL
+  const params = useParams();
+
+  //state
+  const [van, setVan] = React.useState(null);
+
+  //grab the van from the mirage server that matches host id
+  React.useEffect(() => {
+    fetch(`/api/host/vans/${params.id}`)
+      .then((res) => res.json())
+      .then((data) => setVan(data.vans));
+  }, [params.id]);
+
   return (
-    <h1>This is the host single van detail.</h1>
-  )
+    <div className="van-detail-container">
+      {van ? (
+        <div className="van-detail">
+          <img src={van.imageUrl} />
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
+          <h2>{van.name}</h2>
+          <p className="van-price">
+            <span>${van.price}</span>/day
+          </p>
+        </div>
+      ) : (
+        <h2>Loading...</h2>
+      )}
+    </div>
+  );
 }
