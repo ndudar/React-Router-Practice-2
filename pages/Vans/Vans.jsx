feb43be05738ea69d295936c972d0dfb50dac52a
ndudar <ndudar@gmail.com>
2023-03-08 15:47:29 -0600

    feat: setting up filtering for van types

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -16,7 +16,12 @@ export default function Vans() {
       .then((data) => setVans(data.vans));
   }, []);
 
-  const vanElements = vans.map((van) => (
+  //filtering the vans array if we have a typeFilter
+  const displayedVans = typeFilter
+    ? vans.filter(van => van.type === typeFilter)
+    : vans
+
+  const vanElements = displayedVans.map((van) => (
     <div key={van.id} className="van-tile">
       <Link to={`/vans/${van.id}`}>
         <img src={van.imageUrl} />
