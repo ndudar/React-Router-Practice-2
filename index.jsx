129b7ad1c0423df9c39ed6866f055cb26ca39427
ndudar <ndudar@gmail.com>
2023-03-23 15:10:03 -0500

    feat: importing AuthRequired

--- index.jsx
+++ index.jsx
@@ -24,6 +24,7 @@ import HostVanPhotos from "./pages/Host/HostVanPhotos";
 import NotFound from "./pages/NotFound";
 import Error from "./components/Error";
 import Login from "./pages/Login";
+import AuthRequired from "./components/AuthRequired";
 
 import "./server";
 
