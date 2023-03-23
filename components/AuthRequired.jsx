00bfbdf46b634286bf310d13d4697988c15efac3
ndudar <ndudar@gmail.com>
2023-03-23 16:23:12 -0500

    fix: AuthRequired not hardcoded anymore

--- components/AuthRequired.jsx
+++ components/AuthRequired.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { Outlet, Navigate } from "react-router-dom";
 
 export default function AuthRequired() {
-  const auth = { token: "123" };
+  const isLoggedIn = localStorage.getItem("loggedin");
 
-  if (!auth.token) {
+  if (!isLoggedIn) {
     return (
       <Navigate to="/login" state={{ message: "You must log in first." }} />
     );
