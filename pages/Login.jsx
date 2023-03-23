0d2fcf909911e2b855c27cb8e61680deacf52006
ndudar <ndudar@gmail.com>
2023-03-23 15:36:36 -0500

    feat: importing login function

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
+import { loginUser } from "../api";
 
 export default function Login() {
   const [loginFormData, setLoginFormData] = React.useState({
