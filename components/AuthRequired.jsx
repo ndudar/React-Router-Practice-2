bbe88ceb2f8a98a84146674e53e0eed4d4b03c16
ndudar <ndudar@gmail.com>
2023-03-23 15:17:19 -0500

    feat: message state for AuthRequired

--- components/AuthRequired.jsx
+++ components/AuthRequired.jsx
@@ -1,11 +1,13 @@
-import React from "react"
-import { Outlet, Navigate } from "react-router-dom"
+import React from "react";
+import { Outlet, Navigate } from "react-router-dom";
 
 export default function AuthRequired() {
-    const auth = { token: null }
+  const auth = { token: null };
 
-    if (!auth.token) {
-        return <Navigate to="/login" />
-    }
-    return <Outlet />
+  if (!auth.token) {
+    return (
+      <Navigate to="/login" state={{ message: "You must log in first." }} />
+    );
+  }
+  return <Outlet />;
 }
