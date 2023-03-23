4058da345c56045fe7d6c99d2871809a3751e452
ndudar <ndudar@gmail.com>
2023-03-23 16:39:04 -0500

    fix: replace history stack for authrequired

--- components/AuthRequired.jsx
+++ components/AuthRequired.jsx
@@ -6,7 +6,11 @@ export default function AuthRequired() {
 
   if (!isLoggedIn) {
     return (
-      <Navigate to="/login" state={{ message: "You must log in first." }} />
+      <Navigate
+        to="/login"
+        state={{ message: "You must log in first." }}
+        replace
+      />
     );
   }
   return <Outlet />;
