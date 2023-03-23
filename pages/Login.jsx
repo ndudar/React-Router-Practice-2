05887a4f115676c831c65a1748665352cb7adad6
ndudar <ndudar@gmail.com>
2023-03-23 15:54:24 -0500

    feat: error handling and loading features for login

--- pages/Login.jsx
+++ pages/Login.jsx
@@ -8,11 +8,19 @@ export default function Login() {
     password: "",
   });
   const location = useLocation();
+  const [status, setStatus] = React.useState("idle");
+  const [error, setError] = React.useState(null);
 
   function handleSubmit(e) {
     e.preventDefault();
+    setStatus("submitting");
+    setError(null);
     loginUser(loginFormData)
-    .then(data => console.log(data));
+      .catch((err) => setError(err))
+      .then((data) => console.log(data))
+      .finally(() => {
+        setStatus("idle");
+      });
   }
 
   function handleChange(e) {
@@ -26,9 +34,10 @@ export default function Login() {
   return (
     <div className="login-container">
       {location.state?.message && (
-        <h3 className="login-first">{location.state.message}</h3>
+        <h3 className="login-error">{location.state.message}</h3>
       )}
       <h1>Sign in to your account</h1>
+      {error?.message && <h3 className="login-error">{error.message}</h3>}
       <form onSubmit={handleSubmit} className="login-form">
         <input
           name="email"
@@ -44,7 +53,9 @@ export default function Login() {
           placeholder="Password"
           value={loginFormData.password}
         />
-        <button>Log in</button>
+        <button disabled={status === "submitting"}>
+          {status === "submitting" ? "Logging in..." : "Log in"}
+        </button>
       </form>
     </div>
   );
