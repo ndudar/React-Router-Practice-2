210117df38eb739fc51a6f800a39a1efaaa1fc5b
ndudar <ndudar@gmail.com>
2023-03-23 15:35:20 -0500

    feat: adding login function to API

--- api.js
+++ api.js
@@ -1,13 +1,32 @@
 //removed this logic from the useEffect in Vans and placed it here instead
 export async function getVans() {
-  const res = await fetch("/api/vans")
+  const res = await fetch("/api/vans");
   if (!res.ok) {
     throw {
       message: "Failed to fetch vans",
       statusText: res.statusText,
-      status: res.status
-    }
+      status: res.status,
+    };
   }
-  const data = await res.json()
-  return data.vans
+  const data = await res.json();
+  return data.vans;
+}
+
+//login function here
+export async function loginUser(creds) {
+  const res = await fetch("/api/login", {
+    method: "post",
+    body: JSON.stringify(creds),
+  });
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw {
+      message: data.message,
+      statusText: res.statusText,
+      status: res.status,
+    };
+  }
+
+  return data;
 }
