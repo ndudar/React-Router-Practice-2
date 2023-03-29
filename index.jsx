ae107e53186ca4b01a5b64a31b44ae9cbd21bc8f
ndudar <ndudar@gmail.com>
2023-03-29 16:35:48 -0500

    feat: putting loader and action on login path

--- index.jsx
+++ index.jsx
@@ -23,7 +23,10 @@ import HostVanPricing from "./pages/Host/HostVanPricing";
 import HostVanPhotos from "./pages/Host/HostVanPhotos";
 import NotFound from "./pages/NotFound";
 import Error from "./components/Error";
-import Login, { loader as loginLoader, action as loginAction } from "./pages/Login";
+import Login, {
+  loader as loginLoader,
+  action as loginAction,
+} from "./pages/Login";
 import AuthRequired from "./components/AuthRequired";
 
 import "./server";
@@ -33,7 +36,12 @@ const router = createBrowserRouter(
     <Route path="/" element={<Layout />}>
       <Route index element={<Home />} />
       <Route path="about" element={<About />} />
-      <Route path="login" element={<Login />} />
+      <Route
+        path="login"
+        element={<Login />}
+        loader={loginLoader}
+        action={loginAction}
+      />
       <Route
         path="vans"
         errorElement={<Error />}
