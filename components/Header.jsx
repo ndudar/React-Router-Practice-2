40514fd2dee74e4c30237ed81900bf3d4d02c861
ndudar <ndudar@gmail.com>
2023-03-31 16:24:35 -0500

    fix: bug with logout button

--- components/Header.jsx
+++ components/Header.jsx
@@ -40,7 +40,7 @@ export default function Header() {
                         className="login-icon"
                     />
                 </Link>
-                <button onCllick={fakeLogOut}>X</button>
+                <button onClick={fakeLogOut}>X</button>
             </nav>
         </header>
     )
