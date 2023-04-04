6e42e851a8b6523d12db31139907aab306eba14f
ndudar <ndudar@gmail.com>
2023-04-04 14:56:51 -0500

    feat: Suspense in Vans allows for loading display

--- pages/Vans/Vans.jsx
+++ pages/Vans/Vans.jsx
@@ -8,6 +8,10 @@ import {
 } from "react-router-dom";
 import { getVans } from "../../api";
 
+/**
+ * Challenge: Implement Suspense
+ */
+
 export function loader() {
   return defer({ vans: getVans() });
 }
@@ -102,7 +106,9 @@ export default function Vans() {
   return (
     <div className="van-list-container">
       <h1>Explore our van options</h1>
-      <Await resolve={dataPromise.vans}>{renderVanElements}</Await>
+      <React.Suspense fallback={<h2>Loading vans...</h2>}>
+        <Await resolve={dataPromise.vans}>{renderVanElements}</Await>
+      </React.Suspense>
     </div>
   );
 }
