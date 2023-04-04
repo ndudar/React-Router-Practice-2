55008e5a931c59f8ef9af7f4e6b31623a16aaac8
ndudar <ndudar@gmail.com>
2023-04-04 16:35:00 -0500

    feat: incorportating firebase

--- api.js
+++ api.js
@@ -1,3 +1,35 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDpOebl5-feEAYQYUSR8dNZTzBVsRrrr-w",
+  authDomain: "vanlife-d7099.firebaseapp.com",
+  projectId: "vanlife-d7099",
+  storageBucket: "vanlife-d7099.appspot.com",
+  messagingSenderId: "520637820153",
+  appId: "1:520637820153:web:68f84dce80ad2254a20628"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app)
+
+const vansCollectionRef = collection(db, "vans")
+
+//examples of how to get data from firebase below
+//right now, I'm keeping this all on our mirage server
+//I would need to build these collections on firebase
+
+// export async function getVans() {
+//     const querySnapshot = await getDocs(vansCollectionRef)
+//     const dataArr = querySnapshot.docs.map(doc => ({
+//         ...doc.data(),
+//         id: doc.id
+//     }))
+//     console.log(dataArr)
+//     return dataArr
+// }
+
+
 export async function getVans(id) {
   const url = id ? `/api/vans/${id}` : "/api/vans"
   const res = await fetch(url)
